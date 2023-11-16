74e1c985a3b5892940065afb6c7350a43af585ac
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-11-16 16:28:59 -0500

    Supermercados D5
    Cabie el tipo de datos de los codigos con preio a char y finalize el for para saber si el producto tiene descuento

--- Supermercados_D_5.cpp
+++ Supermercados_D_5.cpp
@@ -4,15 +4,15 @@
 int main(){
 
 	
-	long long Numeros_Rifa [] = {894985943020763,
-        				165331255320221,
-        				248386737320130,
-        				183923092220561}; //Preset de los numeros de rifa
+	char Numeros_Rifa [4][20] = {"894985943020763",
+        				"165331255320221",
+        				"248386737320130",
+        				"183923092220561"}; //Preset de los numeros de rifa
 
-    char Numero_Descuento[4][10] = {8949899430,
-	 								7653512593,
-									9383867373,
-									88392904209}; //Preset de los codigos con descuento
+    char Numero_Descuento[4][12] = {"8949899430",
+	 								"7653512593",
+									"9383867373",
+									"88392904209"}; //Preset de los codigos con descuento
 									
 									
     char Codigo_barras[11]; //10 Digitos
@@ -36,14 +36,27 @@ int main(){
 	    scanf("%5s", &Codigo_Almacen);
 	        
 		//Comparativas de Codigo Almacen
-		if (strlen(Codigo_Almacen) != 5) {
-	        printf("\nEl codigo de Almacen debe ser de 5 caracteres.\n");
-	        return 1;
-			}
-			else{
-				if (strlen(Codigo_Almacen, "20",2) != 0)
-			}
-		
+    	if (strlen(Codigo_Almacen) != 5 || strncmp(Codigo_Almacen, "20", 2) != 0) {
+        printf("\nEl código de Almacén debe ser de 5 caracteres y empezar por \"20\".\n");
+        return 1;
+    	}
+    	//Comienza el sistema para revisar si tiene descuentos
+    	printf("\nProducto Registrado.\n");
+    	
+    	int descuento = 0;
+    	
+    	for (int i = 0; i < 4; i++) {
+        	if (strcmp(Codigo_barras, Numero_Descuento[i]) == 0) {
+            	descuento = 1;
+            	break;
+        	}
+    	}
+    	
+    	if (descuento) {
+			printf("\nSu producto tiene 20% de descuento\n");
+		}
+    	
+    
 				
 return 0;
 }
