da09aed3952984671050cac918ae8255772cbd04
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-11-16 13:09:49 -0500

    Supermercados D_5
    Agregue el if donde se revisa que el codigo de barras efectivamente contenga 10 caracteres y meti todo en un while para que en caso de tenenrlos pueda continuar mediante un Continue

--- Supermercados_D_5.cpp
+++ Supermercados_D_5.cpp
@@ -9,26 +9,31 @@ int main(){
         				248386737320130,
         				183923092220561}; //Preset de los numeros de rifa
 
-    int Numero_Descuento[4][10] = {8949899430,
+    char Numero_Descuento[4][10] = {8949899430,
 	 								7653512593,
 									9383867373,
 									88392904209}; //Preset de los codigos con descuento
 									
 									
-    int Codigo_barras; //10 Digitos
-    int Codigo_Almacen; // 5 digitos los dos primeros son 20
-    
-        printf("\nCajeros D5\n");
+    char Codigo_barras[11]; //10 Digitos
+    char Codigo_Almacen[6]; // 5 digitos los dos primeros son 20
+    	while(1) {
+	        printf("\nCajeros D5\n");
+			
+			printf("\nIngrese el codigo de barras\n");
+	        scanf("%10s", &Codigo_barras);
+	        
+	        printf("\nIngrese el codigo del Almacen\n");
+	        scanf("%5s", &Codigo_Almacen);
+	        
+	        if (strlen(Codigo_barras) != 10) {
+	        	printf("\nEl codigo de barras debe ser de 10 caracteres.\n");
+	        	return 1;
+				}
+			else{
+				continue;
+				}
+		}
 		
-		printf("\nIngrese el codigo de barras\n");
-        scanf("%i", &Codigo_barras);
-        
-        printf("\nIngrese el codigo del Almacen\n");
-        scanf("%i", &Codigo_Almacen);
-
-
-    }
-    
-    
 return 0;
 }
